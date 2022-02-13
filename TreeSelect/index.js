3baeda39a2d5a0022f605d918a46b58c3d646503
Dmorel571 <dmorel571@gmail.com>
2022-02-13 11:03:35 -0500

    fix de export sur index.js, ajout des fichiers yaml

--- TreeSelect/index.js
+++ TreeSelect/index.js
@@ -1,14 +1,1 @@
-import Button from '../Button/Button';
-import Card from '../Card/Card';
-import Carousel from '../Carousel/Carousel';
-import Counter from '../Counter/Counter';
-import Datepicker from '../Datepicker/Datepicker';
-import Error404 from '../Error404/Error404';
-import Form from '../Form/Form';
-import Message from '../Message/Message';
-import Notification from '../Notification/Notification';
-import Pagination from '../Pagination/Pagination';
-import Spinner from '../Spinner/Spinner';
-import TreeSelect from '../TreeSelect/TreeSelect';
-
-export {Button, Card, Carousel, Counter, Datepicker, Error404, Form, Message, Notification, Pagination, Spinner, TreeSelect}
+export {default} from './TreeSelect';
